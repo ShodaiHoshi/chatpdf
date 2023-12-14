c91dc5324be57b0951ddba843c50c56bcb1fbc7e
ShodaiHoshi <144403887+ShodaiHoshi@users.noreply.github.com>
2023-12-14 15:05:28 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('Python-test.html')
 
 @app.route('/fileupload', methods=['POST'])
 def upload_file():
